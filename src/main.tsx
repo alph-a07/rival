ac181a86963524ee88aae8d5e0899567c893fa9f
Jeel Patel <itsjeel01@gmail.com>
2026-07-12 19:35:28 +0530

    feat: add JetBrains Mono font as a dependency and import in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/500.css";
 import "./styles/design-tokens.css";
 import { ThemeProvider } from "./theme/ThemeProvider";
 import App from "./app/App";
