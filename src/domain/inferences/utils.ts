4e5909cd7a5e33676b2c59b7c6de8b8e347c24c0
Jeel Patel <itsjeel01@gmail.com>
2026-08-30 16:55:01 +0530

    refactor: update CheckIn import path to use data/schema for consistency

--- src/domain/inferences/utils.ts
+++ src/domain/inferences/utils.ts
@@ -1,6 +1,6 @@
 import { RIVAL_MATH_CONFIG } from "@/domain/config/tuningConstants";
 import type { Gis } from "@/domain/models/Gis";
-import type { Response } from "@/domain/models/CheckIn";
+import type { Response } from "@/data/schema/CheckIn";
 import { ConditionEvaluator } from "./ConditionEvaluator";
 import type {
   AnsweredOption,
